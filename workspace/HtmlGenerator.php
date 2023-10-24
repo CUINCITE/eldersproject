c992beb026a40bce9e8dd890cbf050c3d47f915a
Daniel <easelpainting@gmail.com>
2023-10-24 16:18:49 +0200

    .

--- workspace/HtmlGenerator.php
+++ workspace/HtmlGenerator.php
@@ -31,9 +31,6 @@ class HtmlGenerator
 
     public function isAjax()
     {
-        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            return true;
-        }
-        return false;
+        return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
     }
 }
