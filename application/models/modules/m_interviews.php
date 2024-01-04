1f7e19de9166e0f21692321f0aac7ed304387484
Daniel <easelpainting@gmail.com>
2024-01-04 12:21:02 +0100

    pagination & filters initial

--- application/models/modules/m_interviews.php
+++ application/models/modules/m_interviews.php
@@ -11,18 +11,32 @@ class model_app_pages_modules_interviews extends model_app_pages_modules
 
 	public function updateModel($m,$url)
 	{
+        /*
+			PAGINATION
+		*/
+
+        $items_per_page = 10;
+
+        if (isset($this->settings['get']['page'])
+            && is_numeric($this->settings['get']['page']
+            && !empty($this->settings['get']['partial']))) {
+            $page = $this->settings['get']['page'];
+        } else $page = 1;
 
 		/*
 			FILTERS
 		*/
 
-        $m['filters']['collections'] = $this->parent->getJsonModel('interviewers');
-        $m['filters']['topics'] = $this->parent->getJsonModel('topics');
-        $m['filters']['states'] = $this->parent->getJsonModel('s_states');
+        // filters available to be selected
+        $m['filters']['collections'] = $_SESSION['dict']['collections'];
+        $m['filters']['topics'] = $_SESSION['dict']['topics'];
+        $m['filters']['states'] = $_SESSION['dict']['states'];
 
+
+        //filters in the query
 		$input=[
 			'collection'=>['collections','interviewers'],
-			'theme'=>['topics','topics'],
+			'topic'=>['topics','topics'],
 			'state'=>['states','narrators_states']
 		];
 
@@ -32,9 +46,11 @@ class model_app_pages_modules_interviews extends model_app_pages_modules
 			if (!empty($this->settings['get'][$k]))
 			{
 				$f=$this->getModelFilter($v[0],$this->settings['get'][$k]);
-				if ($f) $filters[$v[1]]=$f;
-			} else $f=[];
-			$m[$k]=$this->filterUpdate($this->parent->dictGet($v[0]),$f);
+				if (!empty($f)) $filters[$v[1]]=$f;
+			}
+
+            if (!empty($f)) $m[$k]=$this->filterUpdate($this->parent->dictGet($v[0]),$f);
+
 		}
 
 		/*
@@ -89,11 +105,18 @@ class model_app_pages_modules_interviews extends model_app_pages_modules
 			Get Interviews
 		*/
 
-        $m['items']=$this->parent->getJsonModel('interviews_list',$filters,false,$sort_model, 10);
+        $m['items']=$this->parent->getJsonModel('interviews_list',$filters,false,$sort_model, [$page, $items_per_page]);
         $m['items']= array_map(function($item) {
             $item['type'] = 'single';
             return $item;
         }, $m['items']);
+
+        /*
+			Load more
+		*/
+
+        $m['load_more'] = $this->getNextPageUri($page);
+
         
 		return $m;
 	}
@@ -125,9 +148,47 @@ class model_app_pages_modules_interviews extends model_app_pages_modules
 
 	private function getModelFilter($dict,$slug)
 	{
-		$item=$this->parent->dictGet($dict,$slug);
-		if ($item) return $item['id'];
+        $slugs = explode(',', $slug);
+        $return_array = [];
+
+        foreach ($slugs as $slug) {
+            $item=$this->parent->dictGet($dict,$slug);
+            if ($item) $return_array[] = $item['id'];
+        }
+
+        if (!empty($return_array)) {
+            return (count($return_array) == 1) ? $return_array[0] : $return_array;
+        }
+
+        return [];
+
 	}
+
+    function getNextPageUri($current_page): string
+    {
+        // Allowed query parameters
+        $allowedParams = ['page', 'sort', 'collection', 'topic', 'state'];
+
+        // Get current URL without query parameters
+        $requestUri = $_SERVER['REQUEST_URI'];
+        $uri = strtok($requestUri, '?');
+
+        // Parse the query parameters manually
+        $queryParams = [];
+        if (strpos($requestUri, '?') !== false) {
+            parse_str(substr($requestUri, strpos($requestUri, '?') + 1), $queryParams);
+        }
+
+        // Filter out unwanted parameters
+        $params = array_intersect_key($queryParams, array_flip($allowedParams));
+
+        // Increment page number
+        $params['page'] = $current_page + 1;
+        $params['partial'] = true;
+
+        // Build the new URL with updated query parameters
+        return $uri . '?' . http_build_query($params);
+    }
 
 
 }
