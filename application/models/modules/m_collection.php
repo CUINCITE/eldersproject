2c2ce9dddd99054e772bf50c77a2946dc1e5ed3f
Daniel <easelpainting@gmail.com>
2024-01-23 11:58:14 +0100

    .

--- application/models/modules/m_collection.php
+++ application/models/modules/m_collection.php
@@ -18,12 +18,59 @@ class model_app_pages_modules_collection extends model_app_pages_modules
 		{
         	$m['items']=$this->parent->getJsonModel('interviews',['interviewers'=>$m['item']['id'],'active'=>1],false,'label');
             $m['modules'] =$this->parent->getJsonModel('collection_modules',['parent' => $m['item']['id'],'active'=>1]);
+
+            foreach ($m['modules'] as $k=>$v) {
+                if ($v['type']['slug'] == 'collection_chapter') {
+                    $m['modules'][$k]['article'] = $this->updateArticle($v['text']);
+                }
+            }
 		}
 
-//        dd($m['modules']);
+//        dd($m);
 		
 		return $m;
 	}
+
+    public function updateArticle($article)
+    {
+        $dom = new DOMDocument();
+        @$dom->loadHTML($article);
+        $xpath = new DOMXPath($dom);
+
+        $elements = [];
+        $lastType = '';
+        foreach ($dom->getElementsByTagName('*') as $node) {
+
+            if ($node->nodeName == 'p') {
+                if($lastType == 'text') {
+                    $elements[count($elements) - 1]['content'] .= '<br>' . $dom->saveHTML($node);
+                } else {
+                    $elements[] = ['type' => 'text', 'content' => $dom->saveHTML($node)];
+                    $lastType = 'text';
+                }
+            }
+            // You can add further elseif here for handling further types e.g.
+           elseif ($node->nodeName == 'blockquote') {
+                $content = $dom->saveHTML($node);
+               $content = strip_tags($content);
+
+
+               $elements[] = ['type' => 'quote', 'content' => $content, 'clip' => ['id' => 6, 'start' => 21, 'end' => 41]];
+               $lastType = 'quote';
+           }
+        }
+
+        return $elements;
+    }
+
+    private function getAudioLinks($string)
+    {
+        $pattern = '/\[AUDIO=(\d+), (\d+:\d+:\d+)-(\d+:\d+:\d+)\]/';
+        preg_match($pattern, $string, $matches);
+
+        return $matches;
+
+    }
 
 
 }
