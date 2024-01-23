9b14fe9d30b5f5475ff3269dad09f56b9edabeca
Daniel <easelpainting@gmail.com>
2024-01-23 17:37:21 +0100

    .

--- application/models/modules/m_collection.php
+++ application/models/modules/m_collection.php
@@ -31,59 +31,54 @@ class model_app_pages_modules_collection extends model_app_pages_modules
 
     public function updateArticle($article, $article_media) {
         $dom = new DOMDocument();
-        @$dom->loadHTML("<body>{$article}</body>");
+        $dom->loadHTML("<body>{$article}</body>");
 
         $elements = [];
         $lastType = '';
         $bodyNode = $dom->getElementsByTagName('body')->item(0);
+
         foreach ($bodyNode->childNodes as $node) {
-            $nodeName = $node->nodeName;
+
             $content = $dom->saveHTML($node);
+            $content = $this->processAudioTag($content);
 
-            // Check for audio tag and replace it with button
-            $pattern = '/\[AUDIO=(\d+), ([0-9:]+)-([0-9:]+)\]/';
-            if (preg_match($pattern, $content, $matches)) {
-                $id = intval($matches[1]);
-                $startHMS = $matches[2];
-                list($h, $m, $s) = explode(':', $startHMS);
-                $startSeconds = $s + ($m * 60) + ($h * 60 * 60);
-                $button = '<button data-audio-player="' . $id . '" data-start="' . $startSeconds . '" class="quote__cassette">[[svg::cassette]]</button>';
-                $content = preg_replace($pattern, $button, $content);
-            }
-
-            if ($nodeName == 'p' || $nodeName == 'blockquote') {
-
-                if (str_contains($content, '<p><img src="/serdelia/public/ckeditor/plugins/uho_media/icons/uho_media.png"></p>')) {
-                    $type = 'image';
-                    $elements[] = ['type' => $type, 'content' => array_shift($article_media)];
-                    $lastType = $type;
-                }
-
-                else {
-                    $content = str_replace('<blockquote>', '<blockquote class="quote quote--big">', $content);
-                    $type = 'text';
-                    if ($lastType == $type && !empty($elements)) {
-                        $elements[count($elements) - 1]['content'] .= '<br>' . $content;
-                    } else {
-                        $elements[] = ['type' => $type, 'content' => $content];
-                    }
-                    $lastType = $type;
-                }
-
+            if (str_contains($content, '<p><img src="/serdelia/public/ckeditor/plugins/uho_media/icons/uho_media.png"></p>')) {
+                $type = 'image';
+                $elements[] = ['type' => $type, 'content' => array_shift($article_media)];
+                $lastType = $type;
             }
 
             else {
-                if ($lastType == 'text' && $node->nodeType === XML_ELEMENT_NODE) {
-                    $elements[count($elements) - 1]['content'] .= '<br>' . $content;
+                $content = str_replace('<blockquote>', '<blockquote class="quote quote--big">', $content);
+                $type = 'text';
+
+                if ($lastType == $type && !empty($elements)) {
+                    end($elements);
+                    $elements[key($elements)]['content'] .= '<br>' . $content;
+                } else {
+                    $elements[] = ['type' => $type, 'content' => $content];
                 }
-                elseif($node->nodeType === XML_ELEMENT_NODE) {
-                    $elements[] = ['type' => 'text', 'content' => $content];
-                    $lastType = 'text';
-                }
+                $lastType = $type;
             }
+
+
         }
 
         return $elements;
+    }
+
+    private function processAudioTag($content): string {
+        // Check for audio tag and replace it with button
+        $pattern = '/\[AUDIO=(\d+), ([0-9:]+)-([0-9:]+)\]/';
+        if (preg_match($pattern, $content, $matches)) {
+            $id = intval($matches[1]);
+            $startHMS = $matches[2];
+            list($h, $m, $s) = explode(':', $startHMS);
+            $startSeconds = $s + ($m * 60) + ($h * 60 * 60);
+            $button = '<button data-audio-player="' . $id . '" data-start="' . $startSeconds . '" class="quote__cassette">[[svg::cassette]]</button>';
+            $content = preg_replace($pattern, $button, $content);
+        }
+        return $content;
     }
 
 }
