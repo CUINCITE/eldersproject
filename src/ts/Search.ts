bbdc775faa18e68bd00772021bcad32e553eb28f
Michał Piechowiak <55598879+Vukain@users.noreply.github.com>
2024-01-29 17:23:58 +0100

    Livesearch test (#72)
    * test: alternative livesearch_wrap height
    
    * test: livesearch adjust

--- src/ts/Search.ts
+++ src/ts/Search.ts
@@ -137,14 +137,12 @@ export class Search {
         this.allLink = this.view.querySelector('.js-livesearch-all');
 
         // set max-height of livesearch wrap - prevent growing outside viewport
-
-        // const height = window.matchMedia('(orientation: landscape)').matches ? Math.min(this.liveList.clientHeight, window.innerHeight * 0.80) : this.liveList.clientHeight;
         const height = Math.min(this.liveList.clientHeight, window.innerHeight * 0.75);
 
         gsap.timeline()
             .to(this.liveList.parentElement, {
                 duration: !this.isLiveShown ? 0.8 : 0,
-                height,
+                // height,
                 ease: easing,
             })
             .fromTo(
