cfa750d310d19b8f9eebb309fb9d8d1cb41dc30e
Mateusz <mateusz@huncwot.com>
2024-02-06 16:47:23 +0100

    cleanup

--- src/ts/animations/scroll/collectionsSimple.ts
+++ src/ts/animations/scroll/collectionsSimple.ts
@@ -7,7 +7,6 @@ export const collectionsSimple = (el: HTMLElement) => {
     const cardsWrap: HTMLElement = el.querySelector('.js-scroll-cards');
     const namesWrap: HTMLElement = el.querySelector('.js-scroll-names');
     const offset = (window.innerHeight - cardsWrap.clientHeight) / 2;
-    const items = el.querySelectorAll('.js-accordeon-item');
 
 
 
@@ -17,11 +16,5 @@ export const collectionsSimple = (el: HTMLElement) => {
         end: 'bottom 92%',
         pin: cardsWrap,
         endTrigger: namesWrap,
-        markers: true,
-    });
-
-    ScrollTrigger.batch(items, {
-        start: 'top 55%',
-        end: 'bottom top',
     });
 };
