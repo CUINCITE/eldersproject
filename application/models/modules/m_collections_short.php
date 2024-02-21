5b91dc23b3a1d4570f93eafa615c9b9af6329e03
Daniel <easelpainting@gmail.com>
2024-02-21 15:17:03 +0100

    collections short

--- application/models/modules/m_collections_short.php
+++ application/models/modules/m_collections_short.php
@@ -14,8 +14,10 @@ class model_app_pages_modules_collections_short extends model_app_pages_modules
 		$m['items']=$this->parent->dictGet('collections');
 		if (!empty($m['params']['home']))
 		{
+            $seed = date('z');
+            mt_srand($seed);
 			shuffle($m['items']);
-			$m['items']=array_slice($m['items'],0,4);
+			$m['items']=array_slice($m['items'],0,5);
 		}        
 		
 		return $m;
