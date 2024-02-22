912774616e970e690051cda45026cf5514712cd3
Mateusz Szweda <mateuszsz@huncwot.com>
2024-02-22 13:55:05 +0100

    refactor polaroidGallery

--- src/ts/components/PolaroidGallery.ts
+++ src/ts/components/PolaroidGallery.ts
@@ -1,7 +1,6 @@
 import { Component } from '../components/Component';
 
 export class PolaroidGallery extends Component {
-
     public isActive = true;
     private wrap: HTMLElement;
     private slides: NodeListOf<HTMLElement>;
@@ -12,15 +11,15 @@ export class PolaroidGallery extends Component {
 
     constructor(protected view: HTMLElement) {
         super(view);
-
         this.wrap = this.view.querySelector('.js-slider-wrap');
         this.slides = this.wrap.querySelectorAll('.js-slide');
         this.captions = this.view.querySelectorAll('.js-caption');
         this.arrowPrev = this.view.querySelector('.js-slider-prev');
         this.arrowNext = this.view.querySelector('.js-slider-next');
-
         this.bind();
     }
+
+
 
     private bind = (): void => {
         this.arrowNext.addEventListener('click', () => this.goTo(this.activeSlideIndex + 1));
@@ -29,54 +28,43 @@ export class PolaroidGallery extends Component {
     };
 
 
+
     private goTo = (index: number, fast?: boolean): void => {
         const direction: number = index > this.activeSlideIndex ? 1 : -1;
-
-        this.showSlide(index, direction, fast);
+        this.showSlide(direction, fast);
     };
 
 
-    private showSlide = (index: number, direction: number, fast?: boolean): void => {
 
-        this.activeSlideIndex = (this.activeSlideIndex += direction);
+    private showSlide = (direction: number, fast?: boolean): void => {
+        this.activeSlideIndex += direction;
 
         const previousSlide = this.slides[this.activeSlideIndex - 1];
         const firstSlide = this.slides[this.activeSlideIndex];
         const secondSlide = this.slides[this.activeSlideIndex + 1];
         const nextSlide = this.slides[this.activeSlideIndex + 2];
 
-        this.captions.forEach((caption: HTMLElement, i: number) => {
-            caption.classList.toggle('is-active', i === this.activeSlideIndex);
-        });
+        const slides = [previousSlide, firstSlide, secondSlide, nextSlide];
+        const classes = ['is-left-outer', 'is-first-visible', 'is-second-visible', 'is-right-outer'];
 
-        console.log(this.captions);
-
-        [previousSlide, firstSlide, secondSlide, nextSlide].forEach((slide: HTMLElement) => {
+        slides.forEach((slide: HTMLElement, i: number) => {
             if (!slide) return;
+
             const prefix = 'is-';
             const slideClassName = slide.className.split(' ').filter(c => !c.startsWith(prefix));
             slide.className = slideClassName.join(' ').trim();
+
+            slide && slide.classList.add(classes[i]);
         });
 
-        previousSlide && previousSlide.classList.add('is-left-outer');
-        firstSlide && firstSlide.classList.add('is-first-visible');
-        secondSlide && secondSlide.classList.add('is-second-visible');
-        nextSlide && nextSlide.classList.add('is-right-outer');
+        this.captions.forEach((caption: HTMLElement, i: number) => caption.classList.toggle('is-active', i === this.activeSlideIndex));
 
         this.updateArrows();
     };
 
-    private updateArrows(): void {
 
-        switch (this.activeSlideIndex) {
-            case 0:
-                this.view.classList.toggle('is-first');
-                break;
-            case this.slides.length - 1:
-                this.view.classList.toggle('is-last');
-                break;
-            default:
-                this.view.classList.remove('is-first', 'is-last');
-        }
+    private updateArrows(): void {
+        this.view.classList.toggle('is-first', this.activeSlideIndex === 0);
+        this.view.classList.toggle('is-last', this.activeSlideIndex === this.slides.length - 1);
     }
 }
