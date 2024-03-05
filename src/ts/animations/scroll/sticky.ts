c3444716a1721679f59fa7c9fe860289e4054ff4
Mateusz <mateusz@huncwot.com>
2024-03-05 14:49:35 +0100

    sticky sticker

--- src/ts/animations/scroll/sticky.ts
+++ src/ts/animations/scroll/sticky.ts
@@ -1,13 +1,12 @@
 import { gsap } from 'gsap/dist/gsap';
 
-export const sticky = el => {
+export const sticky = (el: HTMLElement) => {
     const endTrigger = document.querySelector('#interviews');
 
     gsap.to(el, {
         scrollTrigger: {
-            start: 'top 10%',
-            end: 'top 10%',
-            onUpdate: self => el.classList.toggle('is-hidden', self.progress > 0.90),
+            start: 'top 25px',
+            end: `top ${el.clientHeight + 25}px`,
             pin: el,
             endTrigger,
         },
