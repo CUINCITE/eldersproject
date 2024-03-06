74d927859dbe466cc9038a0492597b3afe31e4b5
Mateusz <mateusz@huncwot.com>
2024-03-06 15:10:16 +0100

    sticky end position

--- src/ts/animations/scroll/sticky.ts
+++ src/ts/animations/scroll/sticky.ts
@@ -6,9 +6,10 @@ export const sticky = (el: HTMLElement) => {
     gsap.to(el, {
         scrollTrigger: {
             start: 'top 25px',
-            end: `top ${el.clientHeight + 25}px`,
+            end: `top ${el.clientHeight + 50}px`,
             pin: el,
             endTrigger,
+            markers: true,
         },
     });
 };
