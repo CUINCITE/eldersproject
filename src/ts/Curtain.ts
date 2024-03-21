108b8958c1fc01e7e7ad48c5e449568b9e8a7fc1
Michal Piechowiak <nilfenheim@gmail.com>
2024-03-21 12:57:17 +0100

    fix: filters scrollbar bug

--- src/ts/Curtain.ts
+++ src/ts/Curtain.ts
@@ -1,5 +1,5 @@
 import { gsap } from 'gsap/dist/gsap';
-import { easing } from './Site';
+import { breakpoint, easing } from './Site';
 
 
 export interface IImages {
@@ -53,7 +53,7 @@ export class Curtain {
                 resolve();
             },
         });
-        gsap.fromTo(this.lead, { xPercent: -120 }, {
+        breakpoint.desktop && gsap.fromTo(this.lead, { xPercent: -120 }, {
             xPercent: 0,
             duration: 0.8,
             delay: 1.2,
