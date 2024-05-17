0121bb5112183d5690cc4b6efb8e4f48805b275f
Lukasz Knasiecki <lukasz@huncwot.com>
2024-05-17 18:17:40 +0200

    cache plugin update

--- serdelia_config/plugins/cache_clear/plugin.php
+++ serdelia_config/plugins/cache_clear/plugin.php
@@ -21,21 +21,23 @@ class serdelia_plugin_cache_clear
         $url = 'https://'.$_SERVER['HTTP_HOST'].'/api/cache_kill';
         if (strpos($_SERVER['HTTP_HOST'],'.lh') || strpos(' '.$_SERVER['HTTP_HOST'],'staging'))
             $url=str_replace('https','http',$url);
-
+            $ii=0;
             $max = 5;
             $r=0;
-            for ($i = 0; $i < $max; $i++) {
+            for ($i = 0; $i < $max; $i++)
+            {
                 $data = _uho_fx::fileCurl($url);
                 if ($data)
                     $data = json_decode($data, true);
+                
                 if (isset($data['result']) && $data['result'])
-                    $r++;
+                    { $r++; $ii+=$data['count']; }
             }
     
             if ($r == $max)
-                $success[] = 'SQL cache has been cleared';
+                $success[] = 'SQL cache has been cleared. Files: '.$ii.' with '.$url;
             else
-                $errors[] = 'Some problems with SQL cache occured';
+                $errors[] = 'Some problems with SQL cache occured (batches: '.$r.', files removed: '.$ii.') with '.$url;
 
 
         $data = ['result' => true, 'success' => $success, 'errors' => $errors];
