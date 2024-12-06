e15b8d7dadac0b5816a2a1b65cc881566f8b0752
Lukasz Knasiecki <lukasz@huncwot.com>
2024-12-06 16:15:10 +0100

    remove back-end cache

--- index.php
+++ index.php
@@ -43,8 +43,9 @@ if ($index['development'])
   ini_set('error_reporting', E_ALL ^ E_NOTICE);
 } else
 {
-  if (substr($_SERVER['REQUEST_URI'],0,5)=='/api/') define("cache",false);
-    else define("cache",true);
+  //if (substr($_SERVER['REQUEST_URI'],0,5)=='/api/') define("cache",false);
+  //  else define("cache",true);
+  define("cache",false);
   define("debug",false);
   ini_set('display_errors', 0);
   ini_set('log_errors', 1);
